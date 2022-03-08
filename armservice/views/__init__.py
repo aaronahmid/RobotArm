ed935dc1d63c7c6bdcd287616eaaa6763db8ad31
krummitz <amidbidee@gmail.com>
2022-03-08 01:11:03 +0100

    updates

---
+++ armservice/views/__init__.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+"""
+Blueprint initialization
+"""
+from flask import Blueprint
+
+# create blueprint to register/mount
+api_views = Blueprint('api_views', __name__, url_prefix='/api')
+
+# api endpoints
+from armservice.views.index import *
---
+++ armservice/views/index.py
@@ -0,0 +1,20 @@
+#!/usr/bin/python3
+"""
+API index views module
+"""
+from armservice.views import api_views
+from flask import jsonify
+
+
+@api_views.route('/status')
+def status():
+    """
+    Returns json response of api status
+
+    Returns:
+        JSON: json object
+    """
+    status = {
+        "status": "OK"
+    }
+    return jsonify(status)
