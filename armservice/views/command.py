5fcb97880e1a027ed043e9c8481f6331f7a63084
krummitz <amidbidee@gmail.com>
2022-03-23 22:27:49 +0100

    added command apis not tested

---
+++ armservice/views/command.py
@@ -0,0 +1,31 @@
+"""
+command handler apis
+"""
+from armservice.views import api_views
+import states
+from flask import (
+    make_response,
+    request,
+    jsonify
+)
+from handlers import CommandHandler 
+
+
+@api_views.route('/commnds', methods=['GET'], strict_slashes=False)
+def list_commands():
+    """
+    returns all available management command
+    """
+    response = CommandHandler().getAllCommands()
+
+    return make_response(jsonify(response), 2)
+
+@api_views.route('/commands/get_entry_command', methods=['GET'], strict_slashes=False)
+def get_entry_command():
+    """
+    returns an entry command if available and the
+    working directory of the current activated state
+    """
+    response = CommandHandler().getEntryCommand()
+
+    return make_response(jsonify(response), 200)
