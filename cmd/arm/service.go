8edba8f4a30745055ce90d0604e52a2151c64237
krumitz <aaronbahmid@gmail.com>
2026-06-14 16:23:34 +0100

    feat: add service directory preparation and Docker support

--- cmd/arm/service.go
+++ cmd/arm/service.go
@@ -1,13 +1,82 @@
 package main
 
 import (
+	"embed"
 	"fmt"
+	"io"
 	"os"
 	"os/exec"
+	"path/filepath"
 
 	"github.com/aaronahmid/robotarm/internal/api"
 	"github.com/spf13/cobra"
 )
+
+//go:embed embedded/Dockerfile embedded/docker-compose.yml
+var embeddedFiles embed.FS
+
+func serviceDir() string {
+	home, _ := os.UserHomeDir()
+	return filepath.Join(home, ".robotarm", "service")
+}
+
+// prepareServiceDir writes the embedded Dockerfile and docker-compose.yml to
+// ~/.robotarm/service/ and copies the current arm binary there so the Docker
+// build context is self-contained regardless of the caller's working directory.
+func prepareServiceDir() error {
+	dir := serviceDir()
+	if err := os.MkdirAll(dir, 0755); err != nil {
+		return fmt.Errorf("creating service dir: %w", err)
+	}
+
+	for _, name := range []string{"Dockerfile", "docker-compose.yml"} {
+		data, err := embeddedFiles.ReadFile("embedded/" + name)
+		if err != nil {
+			return fmt.Errorf("reading embedded %s: %w", name, err)
+		}
+		if err := os.WriteFile(filepath.Join(dir, name), data, 0644); err != nil {
+			return fmt.Errorf("writing %s: %w", name, err)
+		}
+	}
+
+	execPath, err := os.Executable()
+	if err != nil {
+		return fmt.Errorf("resolving executable path: %w", err)
+	}
+	if err := copyFile(execPath, filepath.Join(dir, "arm")); err != nil {
+		return fmt.Errorf("copying binary: %w", err)
+	}
+
+	return nil
+}
+
+func copyFile(src, dst string) error {
+	in, err := os.Open(src)
+	if err != nil {
+		return err
+	}
+	defer in.Close()
+
+	out, err := os.OpenFile(dst, os.O_CREATE|os.O_WRONLY|os.O_TRUNC, 0755)
+	if err != nil {
+		return err
+	}
+	defer out.Close()
+
+	_, err = io.Copy(out, in)
+	return err
+}
+
+func runDockerCompose(args ...string) error {
+	if err := prepareServiceDir(); err != nil {
+		return fmt.Errorf("preparing service directory: %w", err)
+	}
+	cmd := exec.Command("docker-compose", args...)
+	cmd.Dir = serviceDir()
+	cmd.Stdout = os.Stdout
+	cmd.Stderr = os.Stderr
+	return cmd.Run()
+}
 
 var serviceCmd = &cobra.Command{
 	Use:   "service",
@@ -19,8 +88,8 @@ var serviceCmd = &cobra.Command{
 }
 
 var serviceRunCmd = &cobra.Command{
-	Use:   "run",
-	Short: "Run the API service natively (internal use)",
+	Use:    "run",
+	Short:  "Run the API service natively (internal use)",
 	Hidden: true,
 	Run: func(cmd *cobra.Command, args []string) {
 		fmt.Println("Running native API server...")
@@ -28,14 +97,6 @@ var serviceRunCmd = &cobra.Command{
 			fmt.Println("Error starting server:", err)
 		}
 	},
-}
-
-// helper to run docker-compose commands
-func runDockerCompose(args ...string) error {
-	cmd := exec.Command("docker-compose", args...)
-	cmd.Stdout = os.Stdout
-	cmd.Stderr = os.Stderr
-	return cmd.Run()
 }
 
 var serviceStartCmd = &cobra.Command{
--- go.mod
+++ go.mod
@@ -3,14 +3,15 @@ module github.com/aaronahmid/robotarm
 go 1.21
 
 require (
+	github.com/glebarez/sqlite v1.11.0
 	github.com/spf13/cobra v1.10.2
 	gopkg.in/yaml.v3 v3.0.1
+	gorm.io/gorm v1.31.1
 )
 
 require (
 	github.com/dustin/go-humanize v1.0.1 // indirect
 	github.com/glebarez/go-sqlite v1.21.2 // indirect
-	github.com/glebarez/sqlite v1.11.0 // indirect
 	github.com/google/uuid v1.3.0 // indirect
 	github.com/inconshreveable/mousetrap v1.1.0 // indirect
 	github.com/jinzhu/inflection v1.0.0 // indirect
@@ -20,7 +21,6 @@ require (
 	github.com/spf13/pflag v1.0.10 // indirect
 	golang.org/x/sys v0.7.0 // indirect
 	golang.org/x/text v0.20.0 // indirect
-	gorm.io/gorm v1.31.1 // indirect
 	modernc.org/libc v1.22.5 // indirect
 	modernc.org/mathutil v1.5.0 // indirect
 	modernc.org/memory v1.5.0 // indirect
